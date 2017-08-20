b3a8c192a82cb99f9adff50d67ffc71c14d81f39
Abhinav Sharma <abhi18av@gmail.com>
2017-08-20 23:29:09 +0530

    Ruby and it’s tooling does make my job much easier for now

--- src/clj/clojuregem/rbrepl/scratch.rb
+++ src/clj/clojuregem/rbrepl/scratch.rb
@@ -1,4 +1,34 @@
 require 'parser/current'
-
 p Parser::CurrentRuby.parse("2 + 2")
+
+
+require "unparser"
+
+
+
+
+module SExp
+  def s(type, *children)
+    Parser::AST::Node.new(type, children)
+  end
+end
+
+include SExp
+
+
+
+node = s(:def,
+  :foo,
+  s(:args,
+    s(:arg, :x)
+   ),
+  s(:send,
+    s(:lvar, :x),
+    :+,
+    s(:int, 3)
+   )
+)
+
+puts Unparser.unparse(node) # => "def foo(x)\n  x + 3\nend"
+
 
