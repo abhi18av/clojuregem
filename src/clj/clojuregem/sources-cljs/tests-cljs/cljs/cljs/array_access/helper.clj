10d218e698768e7cea3eb24d8c48c83db1bb61d1
Abhinav Sharma <abhi18av@gmail.com>
2017-08-23 11:56:35 +0530

    renamed the clojure tools analyzer pdfd

--- src/clj/clojuregem/sources-cljs/tests-cljs/cljs/cljs/array_access/helper.clj
+++
@@ -1,5 +0,0 @@
-(ns cljs.array-access.helper
-  (:require [cljs.analyzer :as ana]))
-
-(defmacro unchecked-arrays? []
-  (ana/unchecked-arrays?))
--- src/clj/clojuregem/sources-cljs/tests-cljs/cljs/preloads_test/core.cljs
+++
@@ -1,3 +0,0 @@
-(ns preloads-test.core)
-
-(def foo :foo)
