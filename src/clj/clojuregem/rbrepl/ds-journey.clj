45053fc9e42fa668e289a02d23d9c6ec32b70c56
Abhinav Sharma <abhi18av@gmail.com>
2017-08-31 06:52:14 +0530

    starting to work on unraveling the clojure DS philosophy

---
+++ src/clj/clojuregem/rbrepl/ds-journey.clj
@@ -0,0 +1,26 @@
+(ns user
+  (:require [com.rpl.specter :as specter]
+            [clojure.string :as string]
+            [clojure.spec.alpha :as spec]
+            [clojure.test.check.generators :as gen]))
+
+;; LISTS
+
+(def l-1 '(1 2 3 4))
+
+
+(first l-1)
+
+
+
+
+
+
+
+;; VECTORS
+
+
+;; HASH-MAP
+
+
+;; SET
